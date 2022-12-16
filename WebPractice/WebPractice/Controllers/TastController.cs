fd6894878decaee4263babb121f9b15ba16cafb4
atarou <toki19990905@gmail.com>
2022-12-17 03:23:29 +0900

    最初のコミット

---
+++ WebPractice/WebPractice/Controllers/TastController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebPractice.Controllers
+{
+    public class TastController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Solt()
+        {
+            return View();
+        }
+
+        public IActionResult Miso() 
+        {
+            return View();
+        }
+        public IActionResult Pigbones()
+        {
+            return View();
+        }
+        public IActionResult SoySorce()
+        {
+            return View();
+        }
+
+        public IActionResult Others()
+        {
+            return View();
+        }
+    }
+}
