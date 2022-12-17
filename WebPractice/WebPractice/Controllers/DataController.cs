a54e8fd98bc514fb72675f2dcca0f1bbd0cbb3f9
atarou <toki19990905@gmail.com>
2022-12-18 04:11:59 +0900

    各ページ内容追加

---
+++ WebPractice/WebPractice/Controllers/DataController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebPractice.Controllers
+{
+    public class DataController : Controller
+    {
+        public void Insert() 
+        {
+            //登録処理を実施
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
